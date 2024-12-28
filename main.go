0f9c75798ced7dce76a87e1d125f1cbeabbf4780
Krishang <krishang@thirdweb.com>
2024-12-28 09:47:11 +0530

    decode using /abi

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
-	"math/big"
 
 	pgpcrypto "github.com/ProtonMail/gopenpgp/v2/crypto"
 	"github.com/ethereum/go-ethereum/accounts/abi"
@@ -127,7 +126,7 @@ var GpgEd25519VerifyGas uint64 = 2000 // GPG Ed25519 signature verification gas 
 type gpgEd25519Verify struct{}
 
 var (
-	errInputTooShort    = errors.New("input too short")
+	errDecodingFailed    = errors.New("failed to decode input")
 	errInvalidPublicKey   = errors.New("invalid public key")
 )
 
@@ -140,27 +139,60 @@ func (c *gpgEd25519Verify) RequiredGas(input []byte) uint64 {
 // Run performs ed25519 signature verification
 func (c *gpgEd25519Verify) Run(input []byte) ([]byte, error) {
 	// Input should be: abi.encode(bytes32 message, bytes publicKey, bytes signature)
+	
+	decode := func(encodedInput []byte) ([32]byte, []byte, []byte, error) {
+		// Define ABI types
+		bytesType, err := abi.NewType("bytes", "", nil)
+		if err != nil {
+			return [32]byte{}, nil, nil, fmt.Errorf("failed to create bytes type: %v", err)
+		}
+		bytes32Type, err := abi.NewType("bytes32", "", nil)
+		if err != nil {
+			return [32]byte{}, nil, nil, fmt.Errorf("failed to create bytes32 type: %v", err)
+		}
 
-	if len(input) < 96 { // 32 bytes (message) + 32 bytes (publicKey offset) + 32 bytes (signature offset)
-		return nil, errInputTooShort
+		// Create ABI arguments
+		arguments := abi.Arguments{
+			{Type: bytes32Type},
+			{Type: bytesType},
+			{Type: bytesType},
+		}
+
+		// Unpack the encoded data
+		unpacked, err := arguments.Unpack(encodedInput)
+		if err != nil {
+			return [32]byte{}, nil, nil, fmt.Errorf("failed to unpack data: %v", err)
+		}
+
+		// Ensure we have the correct number of elements
+		if len(unpacked) != 3 {
+			return [32]byte{}, nil, nil, fmt.Errorf("unexpected number of decoded arguments: got %d, want 3", len(unpacked))
+		}
+
+		// Extract each value
+		message, ok := unpacked[0].([32]byte)
+		if !ok {
+			return [32]byte{}, nil, nil, fmt.Errorf("failed to cast message to [32]byte")
+		}
+		publicKey, ok := unpacked[1].([]byte)
+		if !ok {
+			return [32]byte{}, nil, nil, fmt.Errorf("failed to cast publicKey to []byte")
+		}
+		signature, ok := unpacked[2].([]byte)
+		if !ok {
+			return [32]byte{}, nil, nil, fmt.Errorf("failed to cast signature to []byte")
+		}
+
+		return message, publicKey, signature, nil
 	}
 
-	// Extract message
-	message := input[:32]
-	messageObj := pgpcrypto.NewPlainMessage(message)
-
-	// Extract public key length and public key
-	publicKeyOffset := int(new(big.Int).SetBytes(input[32 : 64]).Uint64())
-	if len(input) < publicKeyOffset { 
-		return nil, errInputTooShort
+	message, pubKey, signature, err := decode(input)
+	if err != nil {
+		return nil, errDecodingFailed
 	}
 
-	pubKeyLen := int(new(big.Int).SetBytes(input[publicKeyOffset : publicKeyOffset+32]).Uint64())
-	if len(input) < int(publicKeyOffset+32+pubKeyLen) {
-		return nil, errInputTooShort
-	}
-
-	pubKey := input[publicKeyOffset+32 : publicKeyOffset+32+pubKeyLen]
+	// Create message object
+	messageObj := pgpcrypto.NewPlainMessage(message[:])
 
 	// Create public key object
 	pubKeyObj, err := pgpcrypto.NewKey(pubKey)
@@ -173,19 +205,6 @@ func (c *gpgEd25519Verify) Run(input []byte) ([]byte, error) {
 	if err != nil {
 		return nil, errInvalidPublicKey
 	}
-
-	// Extract signature length and signature
-	signatureOffset := int(new(big.Int).SetBytes(input[64:96]).Uint64())
-	if len(input) < signatureOffset { 
-		return nil, errInputTooShort
-	}
-
-	sigLen := int(new(big.Int).SetBytes(input[signatureOffset : signatureOffset+32]).Uint64())
-	if len(input) < int(signatureOffset+32+sigLen) {
-		return nil, errInputTooShort
-	}
-
-	signature := input[signatureOffset+32 : signatureOffset+32+sigLen]
 
 	// Create signature object
 	signatureObj := pgpcrypto.NewPGPSignature(signature)
